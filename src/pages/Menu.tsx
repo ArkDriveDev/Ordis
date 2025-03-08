bbec8ce6f09c58f74b5b6f61d143b3c7cb28d58d
Arky <20231736@gmail.com>
2025-03-08 10:13:03 +0800

    Added menu.tsx wwith logout button and other page route

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,73 @@
+import { 
+    IonButton,
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonIcon, 
+      IonItem, 
+      IonMenu, 
+      IonMenuButton, 
+      IonMenuToggle, 
+      IonPage, 
+      IonRouterOutlet, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react'
+  import {homeOutline, logOutOutline, rocketOutline} from 'ionicons/icons';
+import { Redirect, Route } from 'react-router';
+import Home from './Home';
+import About from './About';
+  const Menu: React.FC = () => {
+    const path = [
+        {name:'Home', url: '/Ordis/app/home', icon: homeOutline},
+        {name:'About', url: '/Ordis-lab/app/about', icon: rocketOutline},
+    ]
+
+    return (
+        <>
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+        {path.map((item,index) =>(
+                            <IonMenuToggle key={index}>
+                                <IonItem routerLink={item.url} routerDirection="forward">
+                                    <IonIcon icon={item.icon} slot="start"></IonIcon>
+                                    {item.name}
+                                </IonItem>
+                            </IonMenuToggle>
+                        ))}
+        <IonButton routerLink="/Ordis" routerDirection="back" expand="full">
+                            <IonIcon icon={logOutOutline} slot="start"> </IonIcon>
+                        Logout
+                        </IonButton>
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+        <IonRouterOutlet id="main">
+        <Route exact path="/Ordis/app/home" component={Home} />
+                    <Route exact path="/Ordis-lab/app/about" component={About} />
+
+                    <Route exact path="/Ordis-lab/app">
+                        <Redirect to="/Ordis-lab/app/home"/>
+                    </Route>
+                </IonRouterOutlet>
+        </IonContent>
+      </IonPage>
+    </>
+    );
+  };
+  
+  export default Menu;
