29d8185c15b4283538305772ccfed8fa079abc76
Arky <20231736@gmail.com>
2025-03-08 10:48:00 +0800

    setup leaflet and installed npm install leaflet react-leaflet@4.2.1 @types/react-leaflet @types/leaflet

--- src/pages/home.tabs/Maps.tsx
+++ src/pages/home.tabs/Maps.tsx
@@ -6,7 +6,13 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
-  
+  import { 
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup
+  } from 'react-leaflet';
+  import "leaflet/dist/leaflet.css"
   const Maps: React.FC = () => {
     return (
       <IonPage>
@@ -18,7 +24,21 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
-          
+          <MapContainer
+          style={{
+            height:"100vh"
+          }}
+          center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+          </MapContainer>
         </IonContent>
       </IonPage>
     );
