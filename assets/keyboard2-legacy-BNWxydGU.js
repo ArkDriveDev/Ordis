f88b2f12ce48717d178ab4bd22167aaee556a4a1
Arky <20231736@gmail.com>
2025-03-08 18:15:07 +0800

    Updates

--- assets/keyboard2-legacy-BNWxydGU.js
+++ assets/keyboard2-legacy-BNWxydGU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DSYJ77lB.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-wyB-WdPB.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
