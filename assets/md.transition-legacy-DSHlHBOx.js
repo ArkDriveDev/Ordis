7df259ed52418317c925a98bf2c269b08ac95538
Arky <20231736@gmail.com>
2025-03-08 09:40:11 +0800

    Updates

--- assets/md.transition-legacy-DSHlHBOx.js
+++ assets/md.transition-legacy-DSHlHBOx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-LHCsItZe.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-pHwUlY1P.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-DxIPrnrh.js
+++ assets/status-tap-legacy-DxIPrnrh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-LHCsItZe.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-pHwUlY1P.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
