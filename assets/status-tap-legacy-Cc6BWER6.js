eb53e84c384c5aee98b10f5a4a6d3784aa413a42
Arky <20231736@gmail.com>
2025-03-09 16:02:48 +0800

    Updates

--- assets/status-tap-legacy-Cc6BWER6.js
+++ assets/status-tap-legacy-Cc6BWER6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BUvt1EGi.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-D9RjjtFb.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-6W4c2u9Q.js
+++ assets/swipe-back-legacy-6W4c2u9Q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BUvt1EGi.js","./index3-legacy-CMCnUnVm.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-D9RjjtFb.js","./index3-legacy-_9yd95As.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
