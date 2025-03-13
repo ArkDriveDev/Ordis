e2351a96b141652f5435558631e97f8bc4257af8
Arky <20231736@nbsc.edu.ph>
2025-03-13 21:06:57 +0800

    Updates

--- assets/swipe-back-legacy-Bs74fqiT.js
+++ assets/swipe-back-legacy-Bs74fqiT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-1PC-EulI.js","./index3-legacy-EKcxQKKG.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-D2rBQQ9a.js","./index3-legacy-ClNHym_L.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
