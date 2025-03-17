e205f47d529645daafcfc49e4874ec5e9375f54d
Arky <20231736@nbsc.edu.ph>
2025-03-17 20:16:41 +0800

    Updates

--- assets/md.transition-legacy-BubCpL81.js
+++ assets/md.transition-legacy-BubCpL81.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-yBd9BNc4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-wCU7rf_u.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
