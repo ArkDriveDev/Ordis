cee99ff1dcad5f33ec5b3b8dd56deaa4e634c7d8
Arky <20231736@nbsc.edu.ph>
2025-03-18 21:50:47 +0800

    Updates

--- assets/ios.transition-legacy-BLCprM8h.js
+++ assets/ios.transition-legacy-BLCprM8h.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QBXaN8uj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BrPjReLx.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BaakxLNq.js
+++ assets/swipe-back-legacy-BaakxLNq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QBXaN8uj.js","./index3-legacy-Bd3xj-xm.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BrPjReLx.js","./index3-legacy-D0IFgVis.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
