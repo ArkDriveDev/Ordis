1ea1c7ed3c7d4df3e7a587b03d079fb1f3fc8d93
JethroBalaga <balagajet@gmail.com>
2025-04-25 19:58:55 +0800

    Updates

--- assets/swipe-back-legacy-DNq5oU2t.js
+++ assets/swipe-back-legacy-DNq5oU2t.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DpnqLpy4.js","./index3-legacy-Dfojr5fn.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-1XfZyY0g.js","./index3-legacy-DLQfbcsE.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
